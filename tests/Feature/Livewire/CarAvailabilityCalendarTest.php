d48b8f08d2035c47a50d2bc8460eb885f2f206d3
Zoltán Tamás Szabó <zoli.szabok@gmail.com>
2025-04-23 10:51:27 +0200

    feat: Simplify CarAvailabilityCalendar mount method and enhance UserFactory with phone number

--- tests/Feature/Livewire/CarAvailabilityCalendarTest.php
+++ tests/Feature/Livewire/CarAvailabilityCalendarTest.php
@@ -1,11 +1,12 @@
 <?php
 
 use App\Livewire\CarAvailabilityCalendar;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Livewire\Livewire;
 
+uses(RefreshDatabase::class);
 it('renders successfully', function () {
-    $this->seed(\Database\Seeders\CarSeeder::class);
-    $this->seed(\Database\Seeders\AvailabilitySeeder::class);
+    $this->seed();
     Livewire::test(CarAvailabilityCalendar::class, ['slug' => 'skoda-octavia-rs', 'displayDays' => 30])
         ->assertStatus(200);
 });
--- tests/Feature/Livewire/CarDetailsTest.php
+++ tests/Feature/Livewire/CarDetailsTest.php
@@ -1,10 +1,14 @@
 <?php
 
 use App\Livewire\CarDetails;
+use Database\Seeders\CarSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Livewire\Livewire;
 
+uses(RefreshDatabase::class);
 it('renders successfully', function () {
-    $this->seed(\Database\Seeders\CarSeeder::class);
+
+    $this->seed(CarSeeder::class);
 
     Livewire::test(CarDetails::class, ['slug' => 'skoda-octavia-rs'])
         ->assertStatus(200);
