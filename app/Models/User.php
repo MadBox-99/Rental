cb003b189428ce6bb4a220c9adb7cbf8831a1aa4
Zoltán Tamás Szabó <zoli.szabok@gmail.com>
2025-04-24 11:43:28 +0200

    fix: Add 'email_verified_at' to mass assignable attributes in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,6 +31,7 @@ class User extends Authenticatable
         'company_registration_number',
         'representative',
         'contact_person',
+        'email_verified_at',
         'phone',
     ];
 
